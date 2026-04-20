e079637aadb2557501e9c4d364b6cfb626e503cd
Ali-Yousuf304 <timeandtrend000@gmail.com>
2026-04-21 00:15:37 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
     vite: {
-        server: {
-             preset: "vercel",
+        // This tells the Nitro engine (used by TanStack Start) to build for Vercel
+        nitro: {
+            preset: "vercel",
         },
     },
 });
