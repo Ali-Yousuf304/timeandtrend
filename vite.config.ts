06421c21ddfec43e1aa3bdb988ae5b56d10afd10
Ali-Yousuf304 <timeandtrend000@gmail.com>
2026-04-21 00:01:55 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from "@tanstack/react-start/config";
-    2 import tsconfigPaths from "vite-tsconfig-paths";
-    3 import tailwindcss from "@tailwindcss/vite";
-    4
-    5 export default defineConfig({
-    6   server: {
-    7     preset: "vercel",
-    8   },
-    9   vite: {
-   10     plugins: [
-   11       tsconfigPaths(),
-   12       tailwindcss(),
-   13     ],
-   14   },
-   15 });
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
+export default defineConfig({
+    vite: {
+        server: {
+             preset: "vercel",
+        },
+    },
+});
